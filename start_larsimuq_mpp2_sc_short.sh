7219e45fd38868a1e0e2d196994e02569f0e6ab2
Ivana Jovanovic <ga45met2@mpp2-login5.cos.lrz.de>
2019-08-13 14:44:03 +0200

    linux cluster trial runs changes

--- start_larsimuq_mpp2_sc_short.sh
+++ start_larsimuq_mpp2_sc_short.sh
@@ -1,6 +1,6 @@
 salloc --ntasks=16 --cpus-per-task=1 \
 mpiexec -genv I_MPI_DEBUG=+5 -print-rank-map python3 uq_simulation.py \
-                            --uq_method "mc" --mc_numevaluations 1000  \
+                            --uq_method "mc" --mc_numevaluations 100 --sc_q_order 4 \
                             --model "larsim" \
                             --chunksize 1 \
                             --mpi --mpi_method "new" \
@@ -8,6 +8,12 @@ mpiexec -genv I_MPI_DEBUG=+5 -print-rank-map python3 uq_simulation.py \
                             --configurationsFile "configuration_larsim.json" \
                             --saltelli \
                             --run_statistics \
-                            --outputResultDir "/naslx/projects/pr63so/ga45met2/Repositories/larsim_runs/" \
-
+                            --outputResultDir "/naslx/projects/pr63so/ga45met2/Repositories/larsim_runs" \
+2>&1 | tee log_larims_trial_run.txt \
 exit
+
+mpiexec -genv I_MPI_DEBUG=+5 -print-rank-map python3 uq_simulation.py --uq_method "mc" --mc_numevaluations 100 --sc_q_order 4 \
+                        --model "larsim" --chunksize 1 --mpi --mpi_method "new" --num_cores 16 --configurationsFile "configuration_larsim.json" \
+                        --saltelli --run_statistics --outputResultDir "/naslx/projects/pr63so/ga45met2/Repositories/larsim_runs" \
+> 2>&1 | tee log_larims_trial_run.txt
+
