06d32ac4fac95de15b8fb577323677c288f70046
Jovanovic <ga45met@atsccs70.informatik.tu-muenchen.de>
2019-11-09 16:23:58 +0100

    Start merging with uqef_new branch

---
+++ uq_simulation_uqsim.py
@@ -0,0 +1,123 @@
+"""
+A simple example for the usage of the UQEF with a test model.
+
+@author: Florian Kuenzner
+"""
+
+# plotting
+import matplotlib
+#matplotlib.use('Agg')
+
+# numerical stuff
+import uqef
+
+import LarsimModel
+import LarsimStatistics
+
+import LinearDampedOscillatorModel
+import LinearDampedOscillatorStatistics
+
+import IshigamiModel
+import IshigamiStatistics
+
+import ProductFunctionModel
+import ProductFunctionStatistics
+
+import os
+import subprocess
+import datetime
+
+# instantiate UQsim
+uqsim = uqef.UQsim()
+
+# change args locally
+local_debugging = False
+if local_debugging:
+    uqsim.args.model = "larsim"
+    uqsim.args.uq_method = "mc"
+    uqsim.args.mc_numevaluations = 10
+    uqsim.args.outputResultDir = "./larsim_runs/"
+    uqsim.args.config_file = "configuration_larsim.json"
+
+    uqsim.setup_configuration_object()
+
+#####################################
+### path settings:
+#####################################
+# each mpi processor has it's own outputResultDir
+if uqsim.is_master():
+    print("path settings...")
+
+if uqsim.args.outputResultDir:
+    rootDir = uqsim.args.outputResultDir
+else:
+    rootDir = os.getcwd()
+
+outputResultDir = os.path.abspath(os.path.join(rootDir, datetime.datetime.now().strftime("%Y-%m-%d:%H:%M")))
+#args.outputResultDir = outputResultDir
+
+if uqsim.is_master():
+    if not os.path.isdir(outputResultDir): subprocess.run(["mkdir", outputResultDir])
+    print("outputResultDir: {}".format(outputResultDir))
+
+#####################################
+### read configuration setting and additional path settings:
+#####################################
+
+if uqsim.is_master():
+    print("xx")
+    print(uqsim.configuration_object)
+
+#Set the working folder where all the model runs related output and files will be written
+try:
+    uqsim.configuration_object["Directories"]["working_dir"] = os.path.abspath(os.path.join(outputResultDir,
+                                                                                            uqsim.configuration_object["Directories"]["working_dir"]))
+except KeyError:
+    try:
+        uqsim.configuration_object["Directories"]["working_dir"] = os.path.abspath(os.path.join(outputResultDir, "model_runs"))
+    except KeyError:
+        uqsim.configuration_object["Directories"] = {}
+        uqsim.configuration_object["Directories"]["working_dir"] = os.path.abspath(os.path.join(outputResultDir, "model_runs"))
+
+if uqsim.is_master():
+    if not os.path.isdir(uqsim.configuration_object["Directories"]["working_dir"]):
+        subprocess.run(["mkdir", uqsim.configuration_object["Directories"]["working_dir"]])
+
+#####################################
+### one time initial model setup
+#####################################
+# put here is there is something specifically related to the model that should be done only once
+if uqsim.is_master():
+    def initialModelSetUp():
+        models = {
+            "larsim"         : (lambda: LarsimModel.LarsimModelSetUp(uqsim.configuration_object))
+           ,"oscillator"     : (lambda: LinearDampedOscillatorModel.LinearDampedOscillatorModelSetUp(uqsim.configuration_object))
+           ,"ishigami"       : (lambda: IshigamiModel.IshigamiModelSetUp(uqsim.configuration_object))
+           ,"productFunction": (lambda: ProductFunctionModel.ProductFunctionModelSetUp(uqsim.configuration_object))
+        }
+        models[uqsim.args.model]()
+    initialModelSetUp()
+
+# register model
+uqsim.models.update({"larsim"         : (lambda: LarsimModel.LarsimModel(uqsim.configuration_object))})
+uqsim.models.update({"oscillator"     : (lambda: LinearDampedOscillatorModel.LinearDampedOscillatorModel(uqsim.configuration_object))})
+uqsim.models.update({"ishigami"       : (lambda: IshigamiModel.IshigamiModel(uqsim.configuration_object))})
+uqsim.models.update({"productFunction": (lambda: ProductFunctionModel.ProductFunctionModel(uqsim.configuration_object))})
+
+# register statistics
+uqsim.statistics.update({"larsim"         : (lambda: LarsimStatistics.LarsimStatistics(uqsim.configuration_object))})
+uqsim.statistics.update({"oscillator"     : (lambda: LinearDampedOscillatorStatistics.LinearDampedOscillatorStatistics())})
+uqsim.statistics.update({"ishigami"       : (lambda: IshigamiStatistics.IshigamiStatistics(uqsim.configuration_object))})
+uqsim.statistics.update({"productFunction": (lambda: ProductFunctionStatistics.ProductFunctionStatistics(uqsim.configuration_object))})
+
+# setup
+uqsim.setup()
+
+# start the simulation
+uqsim.simulate()
+
+# statistics:
+uqsim.calc_statistics()
+uqsim.print_statistics()
+uqsim.plot_statistics(display=True)
+uqsim.save_statistics()
