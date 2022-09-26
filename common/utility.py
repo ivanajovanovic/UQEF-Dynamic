46a6bbb9cf9adc7862eee0615a657b84930016ab
Ivana Jovanovic Buha <ivana.jovanovic@tum.de>
2022-09-26 09:39:23 +0200

    Collect many different function in common/utility.py

--- common/utility.py
+++ common/utility.py
@@ -482,3 +482,248 @@ def calculateGoodnessofFit_simple(measuredDF, predictedDF, gof_list,
             result.append(temp_result)
 
     return result
+
+###################################################################################################################
+# Reading saved data
+###################################################################################################################
+
+def read_configuration_object_json(configurationObject, element=None):
+    """
+    element can be, e.g., "Output", "parameters", "Timeframe", "parameters_settings"
+    """
+    with open(configurationObject) as f:
+        configuration_object = json.load(f)
+    if element is None:
+        return configuration_object
+    else:
+        return configuration_object[element]
+
+
+def read_configuration_object_dill(configurationObject, element=None):
+    """
+    element can be, e.g., "Output", "parameters", "Timeframe", "parameters_settings"
+    """
+    with open(configurationObject, 'rb') as f:
+        configuration_object = dill.load(f)
+    if element is None:
+        return configuration_object
+    else:
+        return configuration_object[element]
+
+
+def read_and_print_uqsim_args_file(file):
+    with open(file, 'rb') as f:
+        uqsim_args = pickle.load(f)
+    uqsim_args_temp_dict = vars(uqsim_args)
+    print(f"UQSIM.ARGS")
+    for key, value in uqsim_args_temp_dict.items():
+        print(f"{key}: {value}")
+    return uqsim_args
+
+
+def _get_df_simulation_from_file(working_folder, df_all_simulations_name="df_all_simulations.pkl"):
+    df_all_simulations = pathlib.Path(working_folder) / df_all_simulations_name
+    df_all_simulations = pd.read_pickle(df_all_simulations, compression='gzip')
+    return df_all_simulations
+
+
+def _get_nodes_from_file(working_folder, dill_or_pickle="dill"):
+    working_folder = paths.pathlib_to_from_str(working_folder, transfrom_to='Path')
+    nodes_dict = working_folder / 'nodes.simnodes'
+    with open(nodes_dict, 'rb') as f:
+        if dill_or_pickle == "dill":
+            simulationNodes = dill.load(f)
+        elif dill_or_pickle == "pickle":
+            simulationNodes = pickle.load(f)
+        else:
+            simulationNodes = None
+    return simulationNodes
+
+
+def _get_df_index_parameter_gof_from_file(
+        working_folder, df_all_index_parameter_gof_values_file_name="df_all_index_parameter_gof_values.pkl"):
+    df_all_index_parameter_gof_values_file = pathlib.Path(working_folder) / df_all_index_parameter_gof_values_file_name
+    df_all_index_parameter_gof_values = pd.read_pickle(df_all_index_parameter_gof_values_file, compression='gzip')
+    return df_all_index_parameter_gof_values
+
+
+def _get_statistics_dict(working_folder, statistics_dictionary_file_name="statistics_dictionary.pkl"):
+    statistics_dictionary_file = pathlib.Path(working_folder) / statistics_dictionary_file_name
+    statistics_dictionary = None
+    if statistics_dictionary_file.is_file():
+        with open(statistics_dictionary_file, 'rb') as f:
+            statistics_dictionary = pickle.load(f)
+    return statistics_dictionary
+
+###################################################################################################################
+# Plotting params and GoF values - mostly from df_index_parameter_gof filtered for a single station
+###################################################################################################################
+
+
+def _get_parameter_columns_df_index_parameter_gof(df_index_parameter_gof):
+    def check_if_column_stores_parameter(x):
+        if isinstance(x, tuple):
+            x = x[0]
+        return not x.startswith("calculate") and not x.startswith("d_calculate") and \
+               x not in ["index_run", "station", "successful_run"]
+    return [x for x in df_index_parameter_gof.columns.tolist() if check_if_column_stores_parameter(x)]
+
+
+def _get_gof_columns_df_index_parameter_gof(df_index_parameter_gof):
+    def check_if_column_stores_gof(x):
+        if isinstance(x, tuple):
+            x = x[0]
+        return x.startswith("calculate") and x not in ["index_run", "station"]
+    return [x for x in df_index_parameter_gof.columns.tolist() if check_if_column_stores_gof(x)]
+
+
+def _get_grad_columns_df_index_parameter_gof(df_index_parameter_gof):
+    return [x for x in df_index_parameter_gof.columns.tolist() if x.startswith("d_calculate")]
+
+
+def plot_hist_of_gof_values_from_df(df_index_parameter_gof, name_of_gof_column="calculateNSE"):
+    return df_index_parameter_gof.hist(name_of_gof_column)
+
+
+def plot_subplot_params_hist_from_df(df_index_parameter_gof):
+    columns_with_parameters = _get_parameter_columns_df_index_parameter_gof(df_index_parameter_gof)
+    fig = make_subplots(rows=1, cols=len(columns_with_parameters))
+    for i in range(len(columns_with_parameters)):
+        if isinstance(columns_with_parameters[i], tuple):
+            fig.append_trace(
+                go.Histogram(
+                    x=df_index_parameter_gof[columns_with_parameters[i]],
+                    name=columns_with_parameters[i][0]
+                ), row=1, col=i + 1)
+        else:
+            fig.append_trace(
+                go.Histogram(
+                    x=df_index_parameter_gof[columns_with_parameters[i]],
+                    name=columns_with_parameters[i]
+                ), row=1, col=i + 1)
+    return fig
+
+
+def plot_subplot_params_hist_from_df_conditioned(df_index_parameter_gof, name_of_gof_column="calculateNSE",
+                                                 threshold_gof_value=0, comparison="smaller"):
+    """
+    comparison should be: "smaller", "greater", "equal"
+    """
+    if comparison == "smaller":
+        mask = df_index_parameter_gof[name_of_gof_column] < threshold_gof_value
+    elif comparison == "greater":
+        mask = df_index_parameter_gof[name_of_gof_column] > threshold_gof_value
+    elif comparison == "equal":
+        mask = df_index_parameter_gof[name_of_gof_column] == threshold_gof_value
+    else:
+        raise Exception("Comparison should be: \"smaller\", \"greater\", \"equal\" ")
+    columns_with_parameters = _get_parameter_columns_df_index_parameter_gof(df_index_parameter_gof)
+    fig = make_subplots(rows=1, cols=len(columns_with_parameters))
+    for i in range(len(columns_with_parameters)):
+        if isinstance(columns_with_parameters[i], tuple):
+            fig.append_trace(
+                go.Histogram(
+                    x=df_index_parameter_gof[mask][columns_with_parameters[i]],
+                    name=columns_with_parameters[i][0]
+                ), row=1, col=i + 1)
+        else:
+            fig.append_trace(
+                go.Histogram(
+                    x=df_index_parameter_gof[mask][columns_with_parameters[i]],
+                    name=columns_with_parameters[i]
+                ), row=1, col=i + 1)
+    return fig
+
+
+def plot_scatter_matrix_params_vs_gof(df_index_parameter_gof, name_of_gof_column="calculateNSE",
+                                      hover_name="index_run"):
+    columns_with_parameters = _get_parameter_columns_df_index_parameter_gof(df_index_parameter_gof)
+    fig = px.scatter_matrix(df_index_parameter_gof,
+                            dimensions=columns_with_parameters,
+                            color=name_of_gof_column,
+                            hover_name=hover_name)
+    fig.update_traces(diagonal_visible=False)
+    return fig
+
+###################################################################################################################
+# Plotting UQ & SA Output
+###################################################################################################################
+
+def scatter_3d_params_vs_gof(df_index_parameter_gof, param1, param2, param3, name_of_gof_column="calculateNSE",
+                             name_of_index_run_column="index_run"):
+    fig = px.scatter_3d(
+        df_index_parameter_gof, x=param1, y=param2, z=param3, color=name_of_gof_column, opacity=0.7,
+        hover_data=[name_of_gof_column, name_of_index_run_column]
+    )
+    return fig
+
+
+def plot_surface_2d_params_vs_gof(df_index_parameter_gof, param1, param2, num_of_points_in_1d=8,
+                                  name_of_gof_column="calculateNSE"):
+    # from mpl_toolkits.mplot3d import Axes3D
+    # from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
+    # from matplotlib import cm
+    # from matplotlib.ticker import LinearLocator, FormatStrFormatter
+    # import matplotlib.pyplot as plt
+
+    # fig = plt.figure(figsize=(10,10))
+    # ax = fig.gca(projection='3d')
+
+    # x = samples_df_index_parameter_gof['A2'].to_numpy()
+    # y = samples_df_index_parameter_gof['BSF'].to_numpy()
+    # z = samples_df_index_parameter_gof['calculateRMSE'].to_numpy()
+
+    # X = np.reshape(x, (-1, 8))
+    # Y = np.reshape(y, (-1, 8))
+    # Z = np.reshape(z, (-1, 8))
+
+    # surf_Rosen = ax.plot_surface(X, Y, Z, cmap=cm.rainbow)
+    # ax.set_xlabel('x')
+    # ax.set_ylabel('y')
+    # ax.set_zlabel('RMSE')
+    # plt.tight_layout()
+    x = df_index_parameter_gof[param1].values
+    y = df_index_parameter_gof[param2].values
+    z = df_index_parameter_gof[name_of_gof_column].values
+    X = np.reshape(x, (-1, num_of_points_in_1d))
+    Y = np.reshape(y, (-1, num_of_points_in_1d))
+    Z = np.reshape(z, (-1, num_of_points_in_1d))
+    fig = go.Figure(data=[go.Surface(x=X, y=Y, z=Z)])
+    fig.update_layout(scene=dict(xaxis_title=param1, yaxis_title=param2, zaxis_title=name_of_gof_column),
+                      autosize=False,
+                      title='GoF Plot',
+                      scene_camera_eye=dict(x=1.87, y=0.88, z=-0.64),
+                      width=900, height=500,
+                      margin=dict(r=20, b=10, l=10, t=10)
+                      )
+    return fig
+
+
+def plot_parallel_params_vs_gof(df_index_parameter_gof, name_of_gof_column="calculateNSE", list_of_params=None):
+    if list_of_params is None:
+        list_of_params = _get_parameter_columns_df_index_parameter_gof(df_index_parameter_gof)
+    dimensions = list_of_params + [name_of_gof_column, ]
+    fig = px.parallel_coordinates(df_index_parameter_gof, color=name_of_gof_column, dimensions=dimensions)
+    return fig
+
+
+def plot_scatter_matrix_params_vs_gof_seaborn(df_index_parameter_gof, name_of_gof_column="calculateNSE"):
+    columns_with_parameters = _get_parameter_columns_df_index_parameter_gof(df_index_parameter_gof)
+    sns.set(style="ticks", color_codes=True)
+    g = sns.pairplot(df_index_parameter_gof,
+                     vars=columns_with_parameters,
+                     corner=True, hue=name_of_gof_column)
+    plt.show()
+
+
+def scatter_3d_params_static(df_index_parameter_gof, param1, param2, param3):
+    fig = plt.figure()
+    axs = fig.gca(projection='3d')
+    axs.scatter(list(df_index_parameter_gof[param1]),
+                list(df_index_parameter_gof[param2]),
+                list(df_index_parameter_gof[param3]), marker="o")
+    plt.title('Parameters\n')
+    axs.set_xlabel(param1)
+    axs.set_ylabel(param2)
+    axs.set_zlabel(param3)
+    plt.show()
